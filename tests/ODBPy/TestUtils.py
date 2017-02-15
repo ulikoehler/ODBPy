6ac820c41f468d80bcc2685c66eeaad607dd453c
Uli Köhler <ulikoehler@online.de>
2017-02-16 00:44:25 +0100

    Added more unit tests

--- tests/ODBPy/TestUtils.py
+++ tests/ODBPy/TestUtils.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-from nose.tools import assert_equal, assert_true, raises, assert_is_none
+from nose.tools import assert_equal, assert_true, assert_false, raises, assert_is_none
 from ODBPy.Utils import *
 
 class TestUtils(object):
@@ -8,4 +8,13 @@ class TestUtils(object):
         assert_equal("01", try_parse_number("01"))
         assert_equal(0, try_parse_number("0"))
         assert_equal(0.1, try_parse_number("0.1"))
+
+    def test_const_false(self):
+        assert_false(const_false())
+
+    def test_not_none(self):
+        assert_false(not_none(None))
+        assert_true(not_none(1))
+        assert_true(not_none(0))
+        assert_true(not_none(""))
 
