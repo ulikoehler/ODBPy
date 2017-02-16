64be582e37170d3af0a83cccae6dade1c8723f55
Uli Köhler <ulikoehler@online.de>
2017-02-16 02:53:28 +0100

    More polygon parser tests

--- tests/ODBPy/TestPolygonParser.py
+++ tests/ODBPy/TestPolygonParser.py
@@ -12,3 +12,37 @@ class TestPolygonParser(object):
             run_decoder_on_line("OB 0 0 I", polygon_decoder_options))
         assert_equal(PolygonBeginTag(Point(3.1, 2.4), PolygonType.Hole),
             run_decoder_on_line("OB 3.1 2.4 H", polygon_decoder_options))
+        assert_is_none(run_decoder_on_line("OB H", polygon_decoder_options))
+
+    def test_parse_os(self):
+        assert_equal(PolygonSegmentTag(Point(0, 0)),
+            run_decoder_on_line("OS 0 0", polygon_decoder_options))
+        assert_equal(PolygonSegmentTag(Point(0, 50)),
+            run_decoder_on_line("OS 0 50", polygon_decoder_options))
+        assert_equal(PolygonSegmentTag(Point(22.5, 15)),
+            run_decoder_on_line("OS 22.5 15", polygon_decoder_options))
+
+        assert_is_none(run_decoder_on_line("OS 22.5", polygon_decoder_options))
+        assert_is_none(run_decoder_on_line("OS", polygon_decoder_options))
+
+    def test_parse_os(self):
+        assert_equal(PolygonCircleTag(Point(0, 0), Point(1, 1), CircleDirection.Clockwise),
+            run_decoder_on_line("OC 0 0 1 1 Y", polygon_decoder_options))
+        assert_equal(PolygonCircleTag(Point(0, 1), Point(2, 3), CircleDirection.Clockwise),
+            run_decoder_on_line("OC 0 1 2 3 Y", polygon_decoder_options))
+        assert_equal(PolygonCircleTag(Point(0.1, 1.2), Point(2.3, 3.4), CircleDirection.Clockwise),
+            run_decoder_on_line("OC 0.1 1.2 2.3 3.4 Y", polygon_decoder_options))
+        assert_equal(PolygonCircleTag(Point(0.1, 1.2), Point(2.3, 3.4), CircleDirection.CounterClockwise),
+            run_decoder_on_line("OC 0.1 1.2 2.3 3.4 N", polygon_decoder_options))
+
+        assert_is_none(run_decoder_on_line("OC", polygon_decoder_options))
+        assert_is_none(run_decoder_on_line("OC 0", polygon_decoder_options))
+        assert_is_none(run_decoder_on_line("OC 0 0", polygon_decoder_options))
+        assert_is_none(run_decoder_on_line("OC 0 0 0", polygon_decoder_options))
+        assert_is_none(run_decoder_on_line("OC 0 0 0 0", polygon_decoder_options))
+        assert_is_none(run_decoder_on_line("OC 0 0 0 0 X", polygon_decoder_options))
+
+    def test_parse_oe(self):
+        assert_equal(PolygonEndTag(), run_decoder_on_line("OE", polygon_decoder_options))
+        assert_is_none(run_decoder_on_line("OE 1", polygon_decoder_options))
+
