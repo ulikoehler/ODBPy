f3ec5c8f4e849b40b66fca2e7cccf43a9128db25
Uli Köhler <ulikoehler@online.de>
2017-02-16 03:47:29 +0100

    Added profile parser text

--- ODBPy/Profile.py
+++ ODBPy/Profile.py
@@ -10,20 +10,16 @@ from .PolygonParser import *
 from .Decoder import *
 from .Treeifier import *
 
-def parse_profile(directory):
+__all__ = ["read_profile", "parse_profile"]
+
+def read_profile(directory):
     profile = read_linerecords(os.path.join(directory, "steps/pcb/profile"))
+    return parse_profile(profile)
+
+def parse_profile(linerecords):
     # Build rulesets
     decoder_options = surface_decoder_options + polygon_decoder_options
     treeifyer_rules = surface_treeify_rules + polygon_treeify_rules
 
-    decoded = list(run_decoder(profile["Layer features"], decoder_options))
+    decoded = list(run_decoder(linerecords["Layer features"], decoder_options))
     return treeify(decoded, treeifyer_rules)
-
-if __name__ == "__main__":
-    #Parse commandline arguments
-    import argparse
-    parser = argparse.ArgumentParser()
-    parser.add_argument("directory", help="The ODB++ directory")
-    args = parser.parse_args()
-    #Perform check
-    print(parse_profile(args.directory))
---
+++ tests/ODBPy/TestProfile.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+from nose.tools import assert_equal, assert_true, assert_false, raises, assert_is_none
+from io import StringIO
+from ODBPy.Profile import *
+from ODBPy.LineRecordParser import *
+from ODBPy.Structures import *
+from ODBPy.SurfaceParser import *
+from ODBPy.PolygonParser import *
+from ODBPy.Decoder import *
+
+testProfile = """
+#
+#Units
+#
+U MM
+
+#
+#Layer features
+#
+S P 0
+OB -38.104 -0.6351 I
+OS -38.104 19.3649
+OS -18.104 19.3649
+OS -18.104 -0.6351
+OS -38.104 -0.6351
+OE
+SE
+"""
+
+class TestProfile(object):
+
+    def test_parse_profile(self):
+        expected = [Surface(Polarity.Positive, 0, [
+            Polygon(PolygonType.Island, [
+                PolygonSegment(start=Point(x=-38.104, y=-0.6351), end=Point(x=-38.104, y=19.3649)),
+                PolygonSegment(start=Point(x=-38.104, y=19.3649), end=Point(x=-18.104, y=19.3649)),
+                PolygonSegment(start=Point(x=-18.104, y=19.3649), end=Point(x=-18.104, y=-0.6351)),
+                PolygonSegment(start=Point(x=-18.104, y=-0.6351), end=Point(x=-38.104, y=-0.6351))
+            ])
+        ], {})]
+        actual = parse_profile(read_linerecords(StringIO(testProfile)))
+        print(actual)
+        assert_equal(expected, actual)
